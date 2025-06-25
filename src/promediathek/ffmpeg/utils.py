acf5ae84b2145c5c5e8de60bf039d1e187e600ad
problemo <problemo@problemo.pro>
2025-06-25 22:43:09 +0200

    Changed check_for_errors to use yuv4mpegpipe and wav as output format because 'null' had some errors with KiKA.

--- src/promediathek/ffmpeg/utils.py
+++ src/promediathek/ffmpeg/utils.py
@@ -125,59 +125,63 @@ def check_for_errors(video_file) -> int:
         print("VERBOSE", "Subtitles can't be checked for errors.")
         return 0
 
-    ffmpeg_out = run([
-        'ffmpeg', '-y', '-loglevel', 'error',
-        '-i', video_file, '-c', 'copy',
-        '-f', 'null', '/dev/null'
-    ], capture_output=True)
-
-    if ffmpeg_out.returncode:
-        log("ERROR", f'FFmpeg Check Error failed. {video_file}')
-        return 1
-
-    elif ffmpeg_out.stderr:
-        log("ERROR", f'FFmpeg Check Error failed. {video_file}')
-        return 2
-
-    ffmpeg_out = run([
-        'ffmpeg', '-y', '-loglevel', 'error',
-        '-i', video_file, '-t', '180',
-        '-f', 'null', '/dev/null'
-    ], capture_output=True)
-
-    if ffmpeg_out.returncode:
-        log("ERROR", f'FFmpeg Check Error failed. {video_file}')
-        return 4
-
-    elif ffmpeg_out.stderr:
-        log("ERROR", f'FFmpeg Check Error failed. {video_file}')
-        return 5
-
-    video_duration = get_duration(ffprobe_out)
-    ffmpeg_out = run([
-        'ffmpeg', '-y', '-loglevel', 'error',
-        '-ss', str(int(video_duration - 180)), '-i', video_file,
-        '-f', 'null', '/dev/null'
-    ], capture_output=True)
-    if ffmpeg_out.returncode:
-        log("ERROR", f'FFmpeg Check Error failed. {video_file}')
-        return 6
-
-    elif ffmpeg_out.stderr:
+    for stream in ffprobe_out['streams']:
+        out_format = "yuv4mpegpipe" if stream['codec_type'] == "video" else "wav"
 
         ffmpeg_out = run([
             'ffmpeg', '-y', '-loglevel', 'error',
-            '-i', video_file,
+            '-i', video_file, '-c', 'copy',
             '-f', 'null', '/dev/null'
         ], capture_output=True)
 
         if ffmpeg_out.returncode:
             log("ERROR", f'FFmpeg Check Error failed. {video_file}')
-            return 7
+            return 1
 
         elif ffmpeg_out.stderr:
             log("ERROR", f'FFmpeg Check Error failed. {video_file}')
-            return 8
+            return 2
+
+        ffmpeg_out = run([
+            'ffmpeg', '-y', '-loglevel', 'error',
+            '-i', video_file, '-t', '180',
+            '-f', out_format, '/dev/null'
+        ], capture_output=True)
+
+        if ffmpeg_out.returncode:
+            log("ERROR", f'FFmpeg Check Error failed. {video_file}')
+            return 4
+
+        elif ffmpeg_out.stderr:
+            log("ERROR", f'FFmpeg Check Error failed. {video_file}')
+            return 5
+
+        video_duration = get_duration(ffprobe_out)
+        ffmpeg_out = run([
+            'ffmpeg', '-y', '-loglevel', 'error',
+            '-ss', str(int(video_duration - 180)), '-i', video_file,
+            '-f', out_format, '/dev/null'
+        ], capture_output=True)
+
+        if ffmpeg_out.returncode:
+            log("ERROR", f'FFmpeg Check Error failed. {video_file}')
+            return 6
+
+        elif ffmpeg_out.stderr:
+
+            ffmpeg_out = run([
+                'ffmpeg', '-y', '-loglevel', 'error',
+                '-i', video_file,
+                '-f', out_format, '/dev/null'
+            ], capture_output=True)
+
+            if ffmpeg_out.returncode:
+                log("ERROR", f'FFmpeg Check Error failed. {video_file}')
+                return 7
+
+            elif ffmpeg_out.stderr:
+                log("ERROR", f'FFmpeg Check Error failed. {video_file}')
+                return 8
 
     return 0
 
